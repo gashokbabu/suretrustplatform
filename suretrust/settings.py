45fc0d866b7e2b11a46703b54d2a3f216c95f9f3
gashokbabu <ashokbabug999@gmail.com>
2021-05-31 12:28:56 +0530

    test5

--- suretrust/settings.py
+++ suretrust/settings.py
@@ -134,9 +134,9 @@ USE_TZ = True
 STATIC_URL = '/static/'
 import os
 
-STATICFILES_DIRS = [
-    (BASE_DIR / 'static'),
-]
+# STATICFILES_DIRS = [
+#     (BASE_DIR / 'static'),
+# ]
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 MEDIA_URL='/media/'
 MEDIA_ROOT = (BASE_DIR / 'media')
