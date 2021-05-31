7ab5d9e4c2b9b524d2838b3dc338a5418ce9084b
gashokbabu <ashokbabug999@gmail.com>
2021-05-31 11:52:51 +0530

    static root congigured with os modele

--- suretrust/settings.py
+++ suretrust/settings.py
@@ -132,7 +132,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+import os
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     (BASE_DIR / 'static'),
 ]
