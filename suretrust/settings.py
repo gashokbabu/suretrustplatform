535ff221e90d039493a457d412d1f338458702f9
gashokbabu <ashokbabug999@gmail.com>
2021-05-31 12:13:19 +0530

    static files errror test2

--- suretrust/settings.py
+++ suretrust/settings.py
@@ -133,7 +133,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 import os
-STATIC_ROOT = (BASE_DIR / 'staticfiles')
+STATIC_ROOT = BASE_DIR / 'static'
 STATICFILES_DIRS = [
     (BASE_DIR / 'static'),
 ]
