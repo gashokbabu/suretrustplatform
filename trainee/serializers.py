438c1847a067d0e2086941abd1910e11023c0ba1
gashokbabu <ashokbabug999@gmail.com>
2021-06-12 19:16:31 +0530

    added user information to students-of-batch api

--- trainee/serializers.py
+++ trainee/serializers.py
@@ -1,7 +1,8 @@
 from rest_framework import serializers
 from .models import Student,ContactUs
-
+from users.serializers import UserSerializer
 class TraineeSerializer(serializers.ModelSerializer):
+    user = UserSerializer(read_only=True)
     class Meta:
         model = Student
         fields = '__all__'
