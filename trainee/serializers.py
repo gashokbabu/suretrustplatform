b99ad14f7e3ed301214c7b5d97e37b84478ad1d0
gashokbabu <ashokbabug999@gmail.com>
2021-06-14 21:14:01 +0530

    Revert "added user information to students-of-batch api"
    This reverts commit 438c1847a067d0e2086941abd1910e11023c0ba1.

--- trainee/serializers.py
+++ trainee/serializers.py
@@ -1,8 +1,7 @@
 from rest_framework import serializers
 from .models import Student,ContactUs
-from users.serializers import UserSerializer
+
 class TraineeSerializer(serializers.ModelSerializer):
-    user = UserSerializer(read_only=True)
     class Meta:
         model = Student
         fields = '__all__'
